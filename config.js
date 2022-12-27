2a3238f785963cff841d33adac73b4363243e894
janiceete <janiceete@gmail.com>
2022-12-27 19:01:24 +0800

    First commit

---
+++ config.js
@@ -0,0 +1,8 @@
+const constants = {
+    openWeatherMap: {
+        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY: "511d6148e790bc30e944cbe669f1fe57"
+    }
+}
+
+module.exports = constants;
